5e46c902c235e03e77b2084c0ee9ee298bb1a0e5
Simon <contact@smn.codes>
2026-03-27 18:55:48 +0100

    feat: tenant selection page

--- src/views/SelectTenantView.tsx
+++ src/views/SelectTenantView.tsx
@@ -1,4 +1,8 @@
-import type { KnownTenant } from '../lib/tenant';
+import { ArrowRightIcon } from 'lucide-react';
+import { buildTenantRoutePath, type KnownTenant } from '../lib/tenant';
+import Hero from '../components/shared/Hero';
+import Link from '../components/ui/elements/Link';
+import TenantMeta from '../components/tenants/TenantMeta';
 
 export type SelectTenantViewProps = {
 	tenants: KnownTenant[];
@@ -6,20 +10,33 @@ export type SelectTenantViewProps = {
 
 export default function SelectTenantView({ tenants }: SelectTenantViewProps) {
 	return (
-		<div>
-			<h1>Please select your tenant to continue.</h1>
-			<ul>
-				{tenants.map((tenant) => (
-					<li key={tenant.id}>
-						<a href={`/id/${tenant.id}`}>{tenant.displayName ?? tenant.id}</a>
-					</li>
-				))}
-				{!tenants.find((t) => t.id === 'default') && (
-					<li key="default">
-						<a href="/id/default">Default Tenant</a>
-					</li>
-				)}
-			</ul>
+		<div className="grow flex flex-col items-center justify-center px-6 py-8">
+			<Hero />
+			<div className="w-[min(100%,400px)] text-center">
+				<p>Please select your tenant to continue:</p>
+				<ul className="mt-4 space-y-4">
+					{tenants.map((tenant) => (
+						<li key={tenant.id}>
+							<Link
+								key={tenant.id}
+								square
+								className="w-full mb-2 flex justify-between gap-3"
+								href={buildTenantRoutePath(tenant.id)}
+								>
+								<TenantMeta knownTenants={tenants} tenantId={tenant.id} />
+								<ArrowRightIcon size={20} className="m-0.5 shrink-0" />
+							</Link>
+						</li>
+					))}
+					<hr className="my-4 border-t border-lm-gray-400 dark:border-dm-gray-600" />
+					{!tenants.find((t) => t.id === 'default') && (
+						<li key="default">
+							<Link variant="link" href={buildTenantRoutePath('default')}>Log in to other tenant</Link>
+						</li>
+					)}
+				</ul>
+			</div>
 		</div>
 	);
 }
+
