07f81e9be1dc0115dc2ce649f9989434455f56e8
Simon <contact@smn.codes>
2026-03-27 18:55:17 +0100

    feat: welcome page

---
+++ src/components/cards/AboutCard.tsx
@@ -0,0 +1,64 @@
+import { BadgeCheckIcon, CalendarDaysIcon, IdCardIcon, SquareStarIcon } from 'lucide-react';
+import laptopPhoneImage from '../../assets/images/laptop-phone.png';
+import Link from '../ui/elements/Link';
+
+const highlights = [
+	{ icon: CalendarDaysIcon, text: 'Proof of age' },
+	{ icon: IdCardIcon, text: 'Government or organizational ID' },
+	{ icon: SquareStarIcon, text: 'Memberships or qualifications' },
+	{ icon: BadgeCheckIcon, text: 'Permissions and authorizations' },
+];
+
+export default function AboutCard() {
+	return (
+		<div className="col-span-full grid md:grid-cols-2 relative p-8 gap-y-8 bg-lm-gray-200 dark:bg-dm-gray-800 rounded-lg border border-lm-gray-400 dark:border-dm-gray-600">
+			<div className="grid grid-rows-[min-content] space-y-4 md:space-y-6 lg:space-y-8">
+				<h2 className="text-2xl font-semibold text-primary dark:text-white">About the SIROS ID Wallet</h2>
+				<div className="space-y-1 text-base">
+					<p>
+						SIROS ID is a digital identity wallet app that lets gives you secure, full control over
+						the storage and use of your credentials, such as:
+					</p>
+					<ul className="my-6 grid grid-cols-2 grid-rows-2 gap-4">
+						{highlights.map(({ icon: Icon, text }) => (
+							<li
+								key={text}
+								className="flex items-center gap-4 text-sm text-primary dark:text-white"
+							>
+								<Icon />
+								{text}
+							</li>
+						))}
+					</ul>
+					<p>
+						Unlike most wallets, SIROS ID Wallet can be used in a web brower or through our native
+						apps, or installed as a progressive web app.
+					</p>
+				</div>
+				<div className="s-hide-in-wrapper-app self-end flex items-center gap-4">
+					<p>Download:</p>
+					<Link variant="link" href="" className="text-primary dark:text-white">
+						App Store
+					</Link>
+					<Link variant="link" href="" className="text-primary dark:text-white">
+						Play Store
+					</Link>
+				</div>
+			</div>
+			<div className="grid grid-rows-[min-content] pb-0 gap-8 justify-items-center">
+				<img
+					src={laptopPhoneImage}
+					alt="Laptop and phone displaying the SIROS ID Wallet app"
+					className="p-4 w-[min(100%,375px)] h-auto object-contain"
+				/>
+				<Link
+					variant="link"
+					href="https://siros.org/how-it-works"
+					className="self-end text-primary dark:text-white"
+				>
+					Learn more about "How do wallets work?"
+				</Link>
+			</div>
+		</div>
+	);
+}
--- src/views/WelcomeView.tsx
+++ src/views/WelcomeView.tsx
@@ -1,7 +1,93 @@
+import { useMemo, type FC } from 'react';
+import useScreenType from '../hooks/useScreenType';
+import Link from '../components/ui/elements/Link';
+import InfoCard, { type InfoCardProps } from '../components/cards/InfoCard';
+import AboutCard from '../components/cards/AboutCard';
+import Hero from '../components/shared/Hero';
+
+type InfoCardItem = InfoCardProps & {
+	Component: FC<InfoCardProps>;
+};
+
+type AboutCardItem = typeof AboutCard;
+
+type CardsList = (InfoCardItem | AboutCardItem)[];
+
+const cardsList: CardsList = [
+	{
+		Component: InfoCard,
+		title: 'Create a wallet account',
+		body: (
+			<>
+				<h3 className="font-semibold">Have an invitation link?</h3>
+				<p>
+					Click on the invitation link sent to you to create a wallet account. If you do not have a
+					link contact your wallet provider.
+				</p>
+				<h3 className="mt-4 font-semibold">Test the SIROS ID Wallet</h3>
+				<p>
+					The SIROS ID wallet is in Beta. Create an account to test out how the technology works.
+				</p>
+			</>
+		),
+		actions: (
+			<>
+				<Link href="/id/default">Continue</Link>
+				<span className="text-primary dark:text-white">
+					Aready have a account?{' '}
+					<Link href="/id/default/login" variant="link" className="text-inherit">
+						Log in
+					</Link>
+				</span>
+			</>
+		),
+	},
+	{
+		Component: InfoCard,
+		title: 'Develop with the SIROS ID Platform',
+		body: (
+			<>
+				<p>
+					Are you an organization interested in using components of the SIROS ID platform and
+					digital identify ecosystem? Use our Issuer, Wallet, Verifier, or trust framework to
+					compliment your solution, or explore our hosted or on prem solutions.
+				</p>
+				<Link variant="link" href="https://developers.siros.org" className="text-primary dark:text-white">
+					Learn more
+				</Link>
+			</>
+		),
+		actions: (
+			<>
+				<Link href="https://siros.org/contact">Contact Us</Link>
+			</>
+		),
+	},
+];
+
 export default function WelcomeView() {
+	const screenType = useScreenType();
+
+	const cards = useMemo(() => {
+		const ordered = [...cardsList];
+		ordered.splice(screenType === 'desktop' ? 2 : 1, 0, AboutCard);
+		return ordered;
+	}, [screenType]);
+
 	return (
-		<div>
-			<h1>Welcome to the Wallet Landing Page</h1>
+		<div className="grow flex flex-col items-center justify-center pt-36 px-6 py-8">
+			<Hero />
+			<div className="mt-8 grid md:grid-cols-2 w-[min(100%,1200px)] gap-x-12 gap-y-8">
+				{cards.map((card, index) => {
+					if (typeof card === 'function') {
+						const Component = card;
+						return <Component key={index} />;
+					} else {
+						const { Component, ...props } = card;
+						return <Component key={props.title} {...props} />;
+					}
+				})}
+			</div>
 		</div>
 	);
 }
