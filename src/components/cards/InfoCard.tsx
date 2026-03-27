0d464b2bcbcb1ff6890225a29ecbf20c22abb05d
Simon <contact@smn.codes>
2026-03-27 19:22:49 +0100

    fix: use container queries to disable flex on small screens

--- src/components/cards/InfoCard.tsx
+++ src/components/cards/InfoCard.tsx
@@ -8,10 +8,10 @@ export type InfoCardProps = {
 
 export default function InfoCard({ title, body, actions }: InfoCardProps) {
 	return (
-		<div className="relative grid grid-rows-[min-content] p-8 space-y-4 md:space-y-6 lg:space-y-8 bg-white rounded-lg dark:bg-dm-gray-900 border border-lm-gray-400 dark:border-dm-gray-600">
+		<div className="@container relative grid grid-rows-[min-content] p-8 space-y-4 md:space-y-6 lg:space-y-8 bg-white rounded-lg dark:bg-dm-gray-900 border border-lm-gray-400 dark:border-dm-gray-600">
 			<h2 className="text-2xl font-semibold">{title}</h2>
 			<div className="space-y-1 text-base">{body}</div>
-			<div className="flex items-center gap-4 self-end">{actions}</div>
+			<div className="@md:flex items-center gap-4 space-y-4 @md:space-y-0 self-end">{actions}</div>
 		</div>
 	);
 }
